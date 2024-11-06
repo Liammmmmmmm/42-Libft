55447e9ea23f75ec132e7e30f7e11f5ef0518891
Liam Lefebvre <lilefebv@z2r9p2.42lyon.fr>
2024-11-06 15:42:40 +0100

    memcmp

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,24 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lilefebv <lilefebv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/06 14:54:59 by lilefebv          #+#    #+#             */
-/*   Updated: 2024/11/06 15:25:46 by lilefebv         ###   ########lyon.fr   */
+/*   Created: 2024/11/06 15:31:38 by lilefebv          #+#    #+#             */
+/*   Updated: 2024/11/06 15:33:07 by lilefebv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_memcmp(const void *s1, const void *s2, unsigned int n)
+char	*strnstr(const char *big, const char *little, size_t len)
 {
-	unsigned int	i;
-
-	i = 0;
-	while (*(((const unsigned char *)s1) + i)
-		&& *(((const unsigned char *)s2) + i)
-		&& *(((const unsigned char *)s1) + i)
-		== *(((const unsigned char *)s2) + i) && i < n)
-		i++;
-	return (*(((char *)s1) + i) - *(((char *)s2) + i));
+	
 }
