1e77bbcc7e6727a686248108499c45bf1f9c2430
Liam Lefebvre <lilefebv@student.42lyon.fr>
2025-04-01 11:20:10 +0200

    FIX: add functions header

--- includes/read_bin.h
+++ includes/read_bin.h
@@ -6,7 +6,7 @@
 /*   By: lilefebv <lilefebv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 15:22:16 by lilefebv          #+#    #+#             */
-/*   Updated: 2025/04/01 11:15:05 by lilefebv         ###   ########lyon.fr   */
+/*   Updated: 2025/04/01 11:20:00 by lilefebv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,22 @@ typedef unsigned char	t_uchar;
 
 typedef struct s_bin
 {
-	t_uchar *data;
+	t_uchar	*data;
 	size_t	size;
 }	t_bin;
 
 int	read_bin_file(t_bin *bin, char *filename);
 
+int	read_uint8(const t_bin *bin, const size_t i, uint8_t *dest);
+int	read_uint8_move(const t_bin *bin, size_t *i, uint8_t *dest);
+
+int	read_uint16(const t_bin *bin, const size_t i, uint16_t *dest);
+int	read_uint16_move(const t_bin *bin, size_t *i, uint16_t *dest);
+
+int	read_uint32(const t_bin *bin, const size_t i, uint32_t *dest);
+int	read_uint32_move(const t_bin *bin, size_t *i, uint32_t *dest);
+
 int	read_uint64(const t_bin *bin, const size_t i, uint64_t *dest);
 int	read_uint64_move(const t_bin *bin, size_t *i, uint64_t *dest);
-
 
 #endif
