8d3ca67cc05e187f377d72123a8881b9d39ca264
Liam Lefebvre <lilefebv@student.42lyon.fr>
2025-06-24 17:00:27 +0200

    NEW: forgot to change in the header

--- includes/read_bin.h
+++ includes/read_bin.h
@@ -6,7 +6,7 @@
 /*   By: lilefebv <lilefebv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 15:22:16 by lilefebv          #+#    #+#             */
-/*   Updated: 2025/06/12 16:21:05 by lilefebv         ###   ########lyon.fr   */
+/*   Updated: 2025/06/24 16:59:52 by lilefebv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ typedef struct s_bin
 	size_t	size;
 }	t_bin;
 
-int		read_bin_file(t_bin *bin, char *filename);
+int		read_bin_file(t_bin *bin, const char *filename);
 
 int		read_uint8(const t_bin *bin, const size_t i, uint8_t *dest);
 int		read_uint8_move(const t_bin *bin, size_t *i, uint8_t *dest);
